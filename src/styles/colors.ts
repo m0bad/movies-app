87434780e609757605480acc2cc6d9abd50892ad
m0bad <mobad.cai@gmail.com>
2021-03-27 15:48:50 +0200

    added colors file.

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+export default {
+  white: '#FFFFFF',
+  radicalRed: '#FF3466',
+  steelGray: '#191926',
+  scarpaFlow: '#565665',
+  stormGray: '#6D6D80',
+  gradientStart: '#8036E7',
+  gradientEnd: '#FF3365',
+}
