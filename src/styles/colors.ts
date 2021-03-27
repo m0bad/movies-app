427ca6646c37bed01541a522c8187069946481d9
m0bad <mobad.cai@gmail.com>
2021-03-27 18:16:41 +0200

    added movies card needed colors.

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,9 +1,12 @@
 export default {
   white: '#FFFFFF',
+  mercury: '#E3E3E3',
   radicalRed: '#FF3466',
   steelGray: '#191926',
+  steelGrayLight: '#222234',
   scarpaFlow: '#565665',
   stormGray: '#6D6D80',
   gradientStart: '#8036E7',
   gradientEnd: '#FF3365',
+  wildWatermelon: '#FF4D79',
 }
