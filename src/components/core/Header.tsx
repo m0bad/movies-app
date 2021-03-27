b92c0a1f3198f5587eed31f4f1f5fa2ec6d5ddcc
m0bad <mobad.cai@gmail.com>
2021-03-27 19:25:55 +0200

    added header core component.

---
+++ src/components/core/Header.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Platform } from 'react-native'
+import styled from 'styled-components/native'
+import { perfectHeight, screenWidth } from 'helpers/responsiveHelpers'
+import colors from 'styles/colors'
+
+type HeaderProps = {
+  children?: React.ReactNode
+}
+
+const Header: React.FC = ({ children }: HeaderProps) => {
+  return (
+    <>
+      <SafeArea />
+      <Container>{children}</Container>
+    </>
+  )
+}
+
+export default Header
+
+const SafeArea = styled.SafeAreaView`
+  width: ${screenWidth}px;
+  background-color: ${colors?.steelGray};
+`
+
+const Container = styled.View`
+  width: ${screenWidth * 0.915}px;
+  align-self: center;
+  background-color: ${colors.steelGray};
+  padding-top: ${Platform.OS === 'android' ? perfectHeight(44) : 0}px;
+`
