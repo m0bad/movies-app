db08fefab480708d73cb8e7f8e86d53976a3c226
m0bad <mobad.cai@gmail.com>
2021-03-29 02:31:36 +0200

    profile text message.

--- src/screens/profile/Profile.tsx
+++ src/screens/profile/Profile.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import ScreenContainer from 'components/layout/ScreenContainer'
+import Header from 'components/core/Header'
+import VerticalSpace from 'components/layout/VerticalSpace'
+import Typography from 'components/core/typography/Typography'
 
 const ProfileScreen: React.FC = () => {
-  return null
+  return (
+    <ScreenContainer widthPercentage={1}>
+      <Header />
+      <VerticalSpace height={300} />
+      <Typography text={'Not implemented Yet!'} textAlign={'center'} size={28} lineHeight={28} />
+    </ScreenContainer>
+  )
 }
 
 export default ProfileScreen
