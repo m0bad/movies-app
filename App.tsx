5ce4179d48614b45de08eb4cd58ab5d0b797ad25
m0bad <mobad.cai@gmail.com>
2021-03-29 21:45:50 +0200

    wrapped the app with react query cache provider.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { QueryClientProvider, QueryClient } from 'react-query'
 import MainNavigator from 'navigators/MainNavigator'
 
-const App = () => {
-  return <MainNavigator />
+const queryClient = new QueryClient()
+
+const App: React.FC = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MainNavigator />
+    </QueryClientProvider>
+  )
 }
 
 export default App
